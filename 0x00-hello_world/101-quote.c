0347c07cd919dc94b3391c9143f3095f102fb698
SouhailFl <souhailfellaki@gmail.com>
2023-03-16 16:22:21 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        write(2,"and that piece of art is useful")
+        return(0);
+}
